07da4706f6768766ad88c9bda2b6db1374cd0241
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-12 14:39:41 -0700

    First Python things

---
+++ Python/ChangingVariables.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# working with variables
+
+myVar = 'hi'
+def readVar():
+  print(myVar)
+def changeLocal():
+  myVar = 'Bye'
+  print(myVar)
+def changeGlobal():
+  global myVar
+  myVar = 'Adios'
+  print(myVar)
+
+readVar()
+changeLocal()
+print(myVar)
+changeGlobal()
+readVar()
+
