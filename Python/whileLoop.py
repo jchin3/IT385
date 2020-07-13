a31bc60a9ca4167bafdb0dae1a4f60ab038c6472
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-12 19:08:27 -0700

    Added While Loop

---
+++ Python/whileLoop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Example while loop
+
+def runLoop():
+  i = 0
+  while i < 10:
+    doWork()
+    i += 1
+
+def doWork():
+  print("All work and no play makes Jack a dull boy")
+
+
+runLoop()
