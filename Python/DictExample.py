3e68c5ac81260e4ca05f64ddafee537a585aa8a7
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 21:13:25 -0700

    Finished dictionary

--- Python/DictExample.py
+++ Python/DictExample.py
@@ -8,3 +8,12 @@ print(eng2esp)
 # View elements
 print(eng2esp["one"])
 print(eng2esp['three'])
+
+# add and remove items from dictionary
+eng2esp.update({"four":"cuatro", "five":"cinco"})
+eng2esp.pop("two")
+print(eng2esp)
+
+# show all keys and values
+print(eng2esp.keys())
+print(eng2esp.values())
