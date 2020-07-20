bc833c6136c1fff6490be88af8c9289d39aa02ee
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 21:57:14 -0700

    finished readfiles

--- Python/readFile.py
+++ Python/readFile.py
@@ -3,11 +3,17 @@
 
 # easy ready of file
 testFile = open("testfile.txt", "r")
-print(testFile.read())
-testFile.close()#!/usr/bin/python3
-# Script to read a file
-
-# easy ready of file
-testFile = open("testfile.txt", "r")
-print(testFile.read())
+print(testFile.read(12))
 testFile.close()
+
+# read file line by line
+testFile = open("testfile.txt", "r")
+for line in testFile:
+  if line.startswith("A"):
+    print(line)
+testFile.close()
+
+# Read file using WITH statement
+with open("testfile.txt", "r")  as testFile:
+  print(testFile.read())
+
