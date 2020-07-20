24893e1ca2760da6e1f1f51eeb06f055573c27c5
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 20:24:18 -0700

    default values

--- Python/functions.py
+++ Python/functions.py
@@ -8,11 +8,27 @@ def func1():
 def addNums(val1, val2):
   print(val1 + val2)
 
+# function that returns a value 
+def cube(x):
+  return x * x * x
+
+# function with default value
+def power(num, power=2):
+  result = 1
+  for i in range(power):
+    result = result * num
+  print(result)
+
 # Call functions
 # func1()
 # print(func1())
 # print(func1)
 
-addNums(5, 6)
-addNums(val2 = 11, val1 = 9)
+# addNums(5, 6)
+# addNums(val2 = 11, val1 = 9)
+  
+# print(cube(5))
 
+power(6,6)
+power(6,2)
+power(6)
