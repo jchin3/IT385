1458fb610dc7d5f83f805411ecbc5b39c998537c
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 21:38:02 -0700

    Writing files

---
+++ Python/writeFile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Example of writing a file
+
+# Open the file
+testFile = open("testfile.txt", "w")
+
+# Write to the file
+testFile.write("Always look on the bright sideof life\n")
+testFile.write("your mother was xxxx and your father smells of elderberries\n")
+
+# Close the file
+testFile.close()
+
