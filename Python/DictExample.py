e4f0f0f45230b1afd114db251b7e2cda458cd12f
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 20:47:11 -0700

    Started dictionary

---
+++ Python/DictExample.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Create dictionary object
+eng2esp = { "one":"uno", "two":"dos", "three":"tres" }
+
+print(eng2esp)
+
+# View elements
+print(eng2esp["one"])
+print(eng2esp['three'])
