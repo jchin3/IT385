15d8e611da5bf1cf16a431ccbd8e64379cbfd0ab
Jung Chin <jchin3@mail.greenriver.edu>
2020-07-19 21:43:20 -0700

    readfiles

---
+++ Python/readFile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Script to read a file
+
+# easy ready of file
+testFile = open("testfile.txt", "r")
+print(testFile.read())
+testFile.close()#!/usr/bin/python3
+# Script to read a file
+
+# easy ready of file
+testFile = open("testfile.txt", "r")
+print(testFile.read())
+testFile.close()
